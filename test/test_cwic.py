7aa8c2b3c7f1eab763d3af95988ec7b22f232853
Aleksandra Zalcman <commandlinegirl@users.noreply.github.com>
2020-05-12 07:40:50 -0700

    DEVEX-1677 cwic: Fix test setup (#342)

--- test/test_cwic.py
+++ test/test_cwic.py
@@ -5,22 +5,20 @@ import dxpy
 import dxpy.api
 import dxpy.app_builder
 from contextlib import contextmanager
-from dxpy.exceptions import DXAPIError
+from dxpy.exceptions import DXAPIError, DXJobFailureError
 
 
 src_dir = os.path.join(os.path.dirname(__file__), "..")
 DX_PROJECT_ID = 'project-BzQf6k80V3bJk7x0yv6z82j7'
 TEST_TIMESTAMP = str(int(time.time()))
 PROJECT_DX = dxpy.DXProject(DX_PROJECT_ID)
-TEST_FOLDER = '/cwic' + TEST_TIMESTAMP
+TEST_FOLDER = '/cwic/test'
 
 class TestCwic(unittest.TestCase):
     applet_id = None
 
     @classmethod
     def setUpClass(cls):
-        print("Creating the folder {}".format(TEST_FOLDER))
-        PROJECT_DX.new_folder(folder=TEST_FOLDER, parents=True)
 
         print("Runing make to create a resource dir")
         dxpy.app_builder.build(src_dir)
@@ -45,11 +43,13 @@ class TestCwic(unittest.TestCase):
  
     @classmethod
     def tearDownClass(cls):
-        print("Clean up by removing the test folder we created")
+        print("Clean up by removing the app we created.")
         try:
-            PROJECT_DX.remove_folder(folder=TEST_FOLDER, recurse=True)
-        except DXAPIError as e:
-            print("Error during cleanup; ignoring.")
+            dxpy.api.container_remove_objects(
+                DX_PROJECT_ID, {"objects": [cls.applet_id]}
+            )
+        except (DXAPIError, DXJobFailureError) as e:
+            print("Error removing {} during cleanup; ignoring.".format(cls.applet_id))
             print(e)
 
     def test_run_no_input_params(self):
@@ -68,26 +68,33 @@ class TestCwic(unittest.TestCase):
             failure_mesg = job.describe()["failureMessage"]
             self.assertIn("Error while checking if cwic will be run interactively", failure_mesg)
 
-    # @unittest.skip(reason="unskip once https://jira.internal.dnanexus.com/browse/DEVEX-1584 is fixed.")
-    # def test_create_file_in_mounted_project(self):
-    #     DX_PROJECT_NAME = PROJECT_DX.describe()["name"]
+    def test_create_file_in_mounted_project(self):
+        """ Test that a file created in a mounted project in cwic is
+        accessible from the platform"""
+        DX_PROJECT_NAME = PROJECT_DX.describe()["name"]
 
-    #     file_name = "test_foo_{}.txt".format(TEST_TIMESTAMP)
-    #     input_args = {
-    #         "cmd": "touch \"/project/{}{}/{}\"".format(DX_PROJECT_NAME, TEST_FOLDER, file_name)
-    #     }
-    #     job = self.applet_dx.run(
-    #         input_args,
-    #         folder=TEST_FOLDER,
-    #         project=DX_PROJECT_ID,
-    #         name=self.applet_basename
-    #     )
-    #     print("Waiting for the job {j_id} to complete".format(j_id=job.get_id()))
-    #     job.wait_on_done()
-    #     list_folder = dxpy.DXProject(DX_PROJECT_ID).list_folder(TEST_FOLDER, describe={"fields": {"name": True}})
-    #     folder_names = [i["describe"]["name"] for i in list_folder["objects"]]
-    #     self.assertIn(file_name, folder_names)
+        file_name = "test_foo_{}.txt".format(TEST_TIMESTAMP)
+        input_args = {
+            "cmd": "touch \"/project/{}{}/{}\"".format(DX_PROJECT_NAME, TEST_FOLDER, file_name)
+        }
+        job = self.applet_dx.run(
+            input_args,
+            folder=TEST_FOLDER,
+            project=DX_PROJECT_ID,
+            name=self.applet_basename
+        )
+        print("Waiting for the job {j_id} to complete".format(j_id=job.get_id()))
+        job.wait_on_done()
+        list_folder = dxpy.DXProject(DX_PROJECT_ID).list_folder(TEST_FOLDER, describe={"fields": {"id": True, "name": True}})
+        file_names = [i["describe"]["name"] for i in list_folder["objects"]]
+        self.assertIn(file_name, file_names)
 
+        # Clean up: get the ID of the created file and remove it
+        f_ids = [f["describe"]["id"] for f in list_folder["objects"] if f["describe"]["name"] == file_name]
+        if f_ids:
+            dxpy.api.container_remove_objects(
+                DX_PROJECT_ID, {"objects": f_ids}
+            )
 
 if __name__ == '__main__':
     unittest.main()
