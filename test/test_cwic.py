ec61187413bb113157698a1dcfff79466585a252
Aleksandra Zalcman <commandlinegirl@users.noreply.github.com>
2020-06-05 07:11:26 -0700

    APPS-65 Add tests reading files in a mounted project and cwic-sub with job token (#355)

--- test/test_cwic.py
+++ test/test_cwic.py
@@ -65,7 +65,19 @@ class TestCwic(unittest.TestCase):
             print("Error removing {} during cleanup; ignoring.".format(cls.applet_id))
             print(e)
 
-    def test_run_no_input_params(self):
+    def test_get_column_widths(self):
+        example_table = [["max-length-13", "short", "short", "same-length-longest"],
+                         ["shorter", "longer-string-another-characters---", "", "same-length-longest"],
+                         ["string", "example", "very-long-string-this-is-longest", "c"],
+                         ["...", "....", ".....", "CAPITAL_CHARS"]]
+        expected_result = [13, 35, 32, 19]
+        expected_result_max_width_20 = [13, 20, 20, 19]
+        assert find_cwic_jobs.get_column_widths(example_table) == expected_result, "max columns widths are different than expected"
+        assert find_cwic_jobs.get_column_widths(example_table, max_width=20) == expected_result_max_width_20, "max columns widths are different than expected"
+
+    def test_run_interactive_mode_without_allowssh(self):
+        """Test that options --ssh or --allow-ssh are used when CWIC
+        is run in an interactive mode"""
         input_args = {}
         job = self.applet_dx.run(
             input_args,
@@ -80,16 +92,6 @@ class TestCwic(unittest.TestCase):
         except dxpy.exceptions.DXJobFailureError:
             failure_mesg = job.describe()["failureMessage"]
             self.assertIn("Error while checking if cwic will be run interactively", failure_mesg)
-
-    def test_get_column_widths(self):
-        example_table = [["max-length-13", "short", "short", "same-length-longest"],
-                         ["shorter", "longer-string-another-characters---", "", "same-length-longest"],
-                         ["string", "example", "very-long-string-this-is-longest", "c"],
-                         ["...", "....", ".....", "CAPITAL_CHARS"]]
-        expected_result = [13, 35, 32, 19]
-        expected_result_max_width_20 = [13, 20, 20, 19]
-        assert find_cwic_jobs.get_column_widths(example_table) == expected_result, "max columns widths are different than expected"
-        assert find_cwic_jobs.get_column_widths(example_table, max_width=20) == expected_result_max_width_20, "max columns widths are different than expected"
 
     def test_create_file_in_mounted_project(self):
         """ Test that a file created in a mounted project in cwic is
@@ -133,6 +135,76 @@ class TestCwic(unittest.TestCase):
         )
         print("Waiting for the job {j_id} to complete".format(j_id=job.get_id()))
         job.wait_on_done()
+
+    def test_read_file_in_mounted_project(self):
+        """ Test that files can be read (with samtools) from a mounted project,
+        dx uploaded, and then read again"""
+
+        bash_script="""
+#!/bin/bash
+  
+set -xe
+proj="DNAnexus Regression Testing Project AWS US east"
+samtools_path="/project/$proj/tools/samtools"
+bam_path="/project/$proj/data_files/Small BAM/SRR504516_small.bam"
+uuid=$(python3 -c 'import uuid; print(uuid.uuid4())')
+
+cp "$samtools_path" /usr/bin/
+chmod +x /usr/bin/samtools
+
+# Removing 20 21 22 since they result in files 100-200M
+chromosomes="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 X Y MT"
+
+#TODO: remove once APPS-66 is completed
+source /home/dnanexus/environment
+unset DX_WORKSPACE_ID
+dx cd $DX_PROJECT_CONTEXT_ID:
+
+# Take md5 sum of each sorted bam and compare to the md5sum
+# when they are read in the dxfuse-mounted project
+for chr in $chromosomes; do
+  bam_name="${uuid}_bam_${chr}.bam"
+  samtools view -b "${bam_path}" "${chr}" -o /scratch/"$bam_name"
+  file_id=$(dx upload /scratch/${bam_name} --brief --wait --destination /cwic/test/)
+  dx-reload-project
+  md5sum_local=($(md5sum /scratch/${bam_name}))
+  md5sum_uploaded=($(md5sum "/project/$proj/cwic/test/$bam_name"))
+  if [ $md5sum_local != $md5sum_uploaded ]; then exit 1; fi
+  dx rm $file_id
+done
+"""
+
+        input_args = {
+            "cmd": bash_script
+        }
+        job = self.applet_dx.run(
+            input_args,
+            folder=TEST_FOLDER,
+            project=DX_PROJECT_ID,
+            name=self.applet_basename
+        )
+        print("Waiting for the job {j_id} to complete".format(j_id=job.get_id()))
+        job.wait_on_done()
+
+    def test_run_sub_without_user_dx_token(self):
+        """Test that dx-cwic-sub exits with an error if user dx token
+        is not provided"""
+        input_args = {
+            "cmd": "dx-cwic-sub 'ls -l /project'"
+        }
+        job = self.applet_dx.run(
+            input_args,
+            folder=TEST_FOLDER,
+            project=DX_PROJECT_ID,
+            name=self.applet_basename
+        )
+        try:
+            job.wait_on_done()
+            # if job continued, fail this test
+            raise Exception("Job should have failed with DXJobFailureError")
+        except dxpy.exceptions.DXJobFailureError:
+            failure_mesg = job.describe()["failureMessage"]
+            self.assertIn("Error", failure_mesg)
 
 if __name__ == '__main__':
     unittest.main()
